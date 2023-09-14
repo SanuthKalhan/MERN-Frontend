a963bca81211490ef792cfd071af0e7385936c52
Mr. S.K.WANNIARACHCHI <136556853+SanuthKalhan@users.noreply.github.com>
2023-09-15 03:51:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./component/Navigation";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { useDispatch, useSelector } from "react-redux";
-import Navigation from './component/Navigation';
-import Home from './pages/Home.js'
-import Login from './pages/Login.js'
-import Signup from './pages/Signup.js';
 import NewProduct from "./pages/NewProduct";
 import ProductPage from "./pages/ProductPage";
 import CategoryPage from "./pages/CategoryPage";
+import ScrollToTop from "./component/ScrollToTop";
 import CartPage from "./pages/CartPage";
 import OrdersPage from "./pages/OrdersPage";
+import AdminDashboard from "./pages/AdminDashboard";
 import EditProductPage from "./pages/EditProductPage";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { addNotification } from "./features/userSlice";
-import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
-  const user = useSelector((state) => state.user);
+    const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
     useEffect(() => {
         const socket = io("ws://localhost:8080");
@@ -35,13 +36,13 @@ function App() {
             }
         });
     }, []);
-  return (
-    <div className="app-background">
-      <div className="App">
-          <BrowserRouter>
-            <Navigation/>
-              <Routes>
-              <Route index element={<Home />} />
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <ScrollToTop />
+                <Navigation />
+                <Routes>
+                    <Route index element={<Home />} />
                     {!user && (
                         <>
                             <Route path="/login" element={<Login />} />
@@ -61,17 +62,17 @@ function App() {
                             <Route path="/product/:id/edit" element={<EditProductPage />} />
                         </>
                     )}
+                    
                     <Route path="/product/:id" element={<ProductPage />} />
                     <Route path="/category/:category" element={<CategoryPage />} />
 
                     <Route path="/new-product" element={<NewProduct />} />
 
                     <Route path="*" element={<Home />} />
-              </Routes>
-          </BrowserRouter>
-      </div>
-    </div>
-  );
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
